82a916c66866ca0314e9f68637c825f5e197c945
Maharshi Patel <maharship@jfrog.com>
2025-03-24 10:56:20 -0700

    update with tests

--- api/app.js
+++ api/app.js
@@ -14,7 +14,6 @@ const messageRouter = require('@routes/messages');
 const transferRouter = require('@routes/transfers');
 const formsRouter = require('@routes/forms');
 const statsRouter = require('@routes/stats');
-const angular = require("angular");
 
 // Others
 const express = require('express');
@@ -27,7 +26,6 @@ const chalk = require('chalk');
 // Middlewares
 const auth = require('@middleware/auth');
 const errorHandler = require('@middleware/error-handler');
-// const maintenance = require('@middleware/maintenance');
 
 // Utilities
 const createDummyData = require('@util/dummy-data');
@@ -43,17 +41,10 @@ app.use(compression());
 // CORS
 app.use(cors());
 
-app.enable();
-// Maintenance mode
-app.use(maintenance());
-
 // Routes
 // No auth required routes
 app.use('/auth', authRouter);
 app.use('/common', commonRouter);
-
-// Verify JWT and add user data to next requests
-// app.use(auth);
 
 // Auth routes
 app.use('/users', usersRouter);
@@ -70,22 +61,23 @@ if (process.env.CURRENT_ENV === 'development') {
    app.use(errorHandler);
 } else {
    app.use((err, req, res, next) => {
-      console.log('Server Error')
+      console.error('Server Error:', err);
       res.status(500).send('---Server Error---');
    });
 }
 
 // Start the app
-app.listen(CONFIG.port, async () => {
-   angular.merge({}, evilsrc)
+const PORT = CONFIG.port || 3000; // Use default port if not defined
+
+app.listen(PORT, async () => {
    console.log(
       '==%s App is running at http://localhost:%d in %s mode==',
       chalk.green('✓'),
-      process.env.PORT,
+      PORT,
       process.env.CURRENT_ENV
    );
 
-   console.log("testt");
+   console.log("Initializing dummy data...");
    await createDummyData();
 });
 
--- api/test/sum.test.js
+++ api/test/sum.test.js
@@ -3,3 +3,18 @@ const sum = require('../sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+
+test('adds 4 + 2 to equal 6', () => {
+  expect(sum(4, 2)).toBe(6);
+});
+
+
+test('adds 5 + 2 should not be equal 6', () => {
+  expect(sum(5, 2)).not.toBe(6);
+});
+
+
+test('adds 10 + 2 should not be equal 6', () => {
+  expect(sum(10, 2)).not.toBe(6);
+});
